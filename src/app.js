62b6236e245dd4601451489a8a2daa76bae4f83c
Esteban <visualdev29@gmail.com>
2025-08-11 20:59:29 -0600

    Function ageCalculator

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 var nombre = "Luis";
 var id = 5;
+function ageCalculator(birthdayStr) {
+    var today = new Date();
+    var birthday = new Date(birthdayStr);
+    var age = today.getFullYear() - birthday.getFullYear();
+    if (today.getMonth() < birthday.getMonth() || (today.getMonth() === birthday.getMonth() && today.getDate() < birthday.getDate())) {
+        age--;
+    }
+    return age;
+}
+var birthday = "2005-03-22";
+console.log("La persona tiene ".concat(ageCalculator(birthday), " a\u00F1os."));
+birthday = "2005-10-22";
+console.log("La persona tiene ".concat(ageCalculator(birthday), " a\u00F1os."));
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
 const nombre: string = "Luis";
 const id: number = 5;
+
+function ageCalculator(birthdayStr: string): number {
+    const today: Date = new Date();
+    const birthday: Date = new Date(birthdayStr);
+
+    let age: number = today.getFullYear() - birthday.getFullYear();
+    
+    if (today.getMonth() < birthday.getMonth() || (today.getMonth() === birthday.getMonth() && today.getDate() < birthday.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+let birthday: string = "2005-03-22";
+
+console.log(`La persona tiene ${ageCalculator(birthday)} años.`);
+
+birthday = "2005-10-22";
+
+console.log(`La persona tiene ${ageCalculator(birthday)} años.`);
